cafd1e5ab58edbe19d5acbf6e03d470e1a44c2c9
jacqueline-homan <jacquelinehoman7@gmail.com>
2015-06-21 12:17:38 -0400

    first commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module DiversityAlgorithm.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("DiversityAlgorithm")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("Codecatenation, LLC")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
